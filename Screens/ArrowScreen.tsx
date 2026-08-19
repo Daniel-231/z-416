a614a0f7a6bd23c3e32efdd12834572678ce8757
Daniel-231 <daniel20040426@gmail.com>
2026-08-19 16:58:06 +0300

    Small Notes

--- Screens/ArrowScreen.tsx
+++ Screens/ArrowScreen.tsx
@@ -1,14 +1,17 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Canvas } from '@react-three/fiber';
 import * as THREE from 'three';
 
-function Arrow() {
+function Object3D() {
     const groupRef = useRef<THREE.Group>(null);
+    useEffect(() => { // If this is meant to update every frame, it's useFrame, not useEffect
+        groupRef.current?.rotation.set(200, 100, 100); // Rotation of the arrow in radians (x, y, z)
+    }, []);
     return (
         <group ref={groupRef}>
             <mesh position={[0, -0.3, 0]}>
-                <cylinderGeometry args={[0.1, 0.1, 0.6, 16]} />
+                <sphereGeometry args={[0.5, 32, 16]} /> {/* Each Shape Has It's Own Args */}
                 <meshStandardMaterial color="#5339e6" />
             </mesh>
         </group>
@@ -19,9 +22,9 @@ const ArrowScreen: React.FC = () => {
     return (
         <View style={styles.container}>
             <Canvas camera={{ position: [0, 0, 3] }}>
-                <ambientLight intensity={0.6} />
+                <ambientLight intensity={0.5} />
                 <directionalLight position={[2, 2, 2]} intensity={1} />
-                <Arrow />
+                <Object3D />
             </Canvas>
         </View>
     );
